142c90b10d3698b30688452f401f133cf62481e5
张海龙 <381266395@qq.com>
2019-07-05 20:58:08 +0800

    add bg music

--- app/src/main/java/com/ball/project/MainActivity.java
+++ app/src/main/java/com/ball/project/MainActivity.java
@@ -1,5 +1,6 @@
 package com.ball.project;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import com.ball.project.tagcloud.TagCloudView;
@@ -10,13 +11,37 @@ public class MainActivity extends AppCompatActivity {
 
     private TagCloudView mTagCloudView;
 
+    private MediaPlayer mMediaPlayer;
+
+    private int[] mMusicList = {R.raw.soulmatch1, R.raw.soulmatch2, R.raw.soulmatch3};
+    private int mPlayMusicCount;
+    private TagCloudAdapter mTagCloudAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mTagCloudView = findViewById(R.id.tag_cloud);
         mTagCloudView.setBackgroundColor(CommonUtils.getColor(R.color.colorTheme));
-        TagCloudAdapter adapter = new TagCloudAdapter(new String[100]);
-        mTagCloudView.setAdapter(adapter);
+        mTagCloudAdapter = new TagCloudAdapter(new String[100]);
+        mTagCloudView.setAdapter(mTagCloudAdapter);
+        startMusic();
+    }
+
+    public void startMusic() {
+        int musicPosition = mPlayMusicCount % 3;
+        if (mMediaPlayer != null) {
+            mMediaPlayer.stop();
+        }
+        mMediaPlayer = MediaPlayer.create(this, mMusicList[musicPosition]);
+        mMediaPlayer.setLooping(false);
+        mMediaPlayer.start();
+        mMediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mPlayMusicCount++;
+                startMusic();
+            }
+        });
     }
 }
